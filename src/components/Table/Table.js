34ac95f9d821f0c174f859b1cf375889ec0b86db
bakhaw <faissal.hattou@gmail.com>
2018-01-24 02:14:05 +0100

    pull

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
-import apiKey from '../../key'
+import apiKey from "../../key";
 
-import CircularProgress from 'material-ui/CircularProgress';
-
+import CircularProgress from "material-ui/CircularProgress";
 
 import {
   Table,
@@ -21,7 +20,7 @@ class LeagueTable extends Component {
     super(props);
     this.state = {
       fetched: false,
-      table: [],
+      table: []
     };
   }
 
@@ -33,40 +32,65 @@ class LeagueTable extends Component {
       }
     };
 
-    const res = await axios.get(`http://api.football-data.org/v1/competitions/${this.props.teamUrl}/leagueTable/`,
-      config
-    );
-    const table = await res.data;
-    this.setState({ table: [table], fetched: true });
+    try {
+      const res = await axios.get(`http://api.football-data.org/v1/competitions/${this.props.teamUrl}/leagueTable/`, config);
+      const table = await res.data;
+      this.setState({ table: [table], fetched: true });
+    } catch (error) {
+      console.log(error.response);
+    }
   }
 
   render() {
     return (
       <div>
-
-        {!this.state.fetched &&
+        {!this.state.fetched && (
           <div className="loading">
-            <CircularProgress thickness={5} color="black"/>
+            <CircularProgress thickness={5} color="black" />
           </div>
-        }
+        )}
 
         {this.state.fetched && (
           <ul>
             <li>
-              {console.log(`http://api.football-data.org/v1/competitions/${this.props.teamUrl}/leagueTable`)}
+              {console.log(
+                `http://api.football-data.org/v1/competitions/${
+                  this.props.teamUrl
+                }/leagueTable`
+              )}
               <Table>
                 <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
                   <TableRow>
-                    <TableHeaderColumn className="posRow">Pos</TableHeaderColumn>
-                    <TableHeaderColumn className="teamRow">Équipe</TableHeaderColumn>
-                    <TableHeaderColumn className="pointsRow">Pts</TableHeaderColumn>
-                    <TableHeaderColumn className="playedRow">Joués</TableHeaderColumn>
-                    <TableHeaderColumn className="winRow">Victoires</TableHeaderColumn>
-                    <TableHeaderColumn className="drawRow">Nuls</TableHeaderColumn>
-                    <TableHeaderColumn className="loseRow">Défaites</TableHeaderColumn>
-                    <TableHeaderColumn className="goalsRow">Pour</TableHeaderColumn>
-                    <TableHeaderColumn className="goalsRow">Contre</TableHeaderColumn>
-                    <TableHeaderColumn className="goalsRow">Différence</TableHeaderColumn>
+                    <TableHeaderColumn className="posRow">
+                      Pos
+                    </TableHeaderColumn>
+                    <TableHeaderColumn className="teamRow">
+                      Équipe
+                    </TableHeaderColumn>
+                    <TableHeaderColumn className="pointsRow">
+                      Pts
+                    </TableHeaderColumn>
+                    <TableHeaderColumn className="playedRow">
+                      Joués
+                    </TableHeaderColumn>
+                    <TableHeaderColumn className="winRow">
+                      Victoires
+                    </TableHeaderColumn>
+                    <TableHeaderColumn className="drawRow">
+                      Nuls
+                    </TableHeaderColumn>
+                    <TableHeaderColumn className="loseRow">
+                      Défaites
+                    </TableHeaderColumn>
+                    <TableHeaderColumn className="goalsRow">
+                      Pour
+                    </TableHeaderColumn>
+                    <TableHeaderColumn className="goalsRow">
+                      Contre
+                    </TableHeaderColumn>
+                    <TableHeaderColumn className="goalsRow">
+                      Différence
+                    </TableHeaderColumn>
                   </TableRow>
                 </TableHeader>
               </Table>
@@ -76,19 +100,42 @@ class LeagueTable extends Component {
                 <Table>
                   <TableBody displayRowCheckbox={false} showRowHover={true}>
                     <TableRow>
-                      <TableRowColumn className="posRow">{data.position}</TableRowColumn>
-                      <TableRowColumn className="teamRow">
-                        <img src={data.crestURI} alt="écusson" className="imgRow"/> {data.teamName}
+                      <TableRowColumn className="posRow">
+                        {data.position}
                       </TableRowColumn>
-                      <TableRowColumn className="pointsRow">{data.points}</TableRowColumn>
-                      <TableRowColumn className="playedRow">{data.playedGames}</TableRowColumn>
-                      <TableRowColumn className="winRow">{data.wins}</TableRowColumn>
-                      <TableRowColumn className="drawRow">{data.draws}</TableRowColumn>
-                      <TableRowColumn className="loseRow">{data.losses}</TableRowColumn>
-                      <TableRowColumn className="goalsRow">{data.goals}</TableRowColumn>
-                      <TableRowColumn className="goalsRow">{data.goalsAgainst}</TableRowColumn>
+                      <TableRowColumn className="teamRow">
+                        <img
+                          src={data.crestURI}
+                          alt="écusson"
+                          className="imgRow"
+                        />{" "}
+                        {data.teamName}
+                      </TableRowColumn>
+                      <TableRowColumn className="pointsRow">
+                        {data.points}
+                      </TableRowColumn>
+                      <TableRowColumn className="playedRow">
+                        {data.playedGames}
+                      </TableRowColumn>
+                      <TableRowColumn className="winRow">
+                        {data.wins}
+                      </TableRowColumn>
+                      <TableRowColumn className="drawRow">
+                        {data.draws}
+                      </TableRowColumn>
+                      <TableRowColumn className="loseRow">
+                        {data.losses}
+                      </TableRowColumn>
                       <TableRowColumn className="goalsRow">
-                        {data.goalDifference > 0 ? `+ ${data.goalDifference}` : data.goalDifference}
+                        {data.goals}
+                      </TableRowColumn>
+                      <TableRowColumn className="goalsRow">
+                        {data.goalsAgainst}
+                      </TableRowColumn>
+                      <TableRowColumn className="goalsRow">
+                        {data.goalDifference > 0
+                          ? `+ ${data.goalDifference}`
+                          : data.goalDifference}
                       </TableRowColumn>
                     </TableRow>
                   </TableBody>
